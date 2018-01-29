18b4817b4a1125d3cc4a7ccb40d75f5e0e5309c2
umasridevi <sridevikamaraj327@gmail.com>
2018-01-29 10:00:07 -0800

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,23 @@
+package ss;
+abstract class GeneralBank
+{
+	double amount;
+	GeneralBank(double x)
+	{
+		amount=x;
+		
+	}
+	abstract double                                                                                                                                                                                                                                                                                    
+	                 
+}
+
+
+public class Bank {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
